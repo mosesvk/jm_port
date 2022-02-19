75da36b174ae8128812790a293fb6032eb0cf2a7
mosesvk <kaumatule93@gmail.com>
2022-02-19 15:45:02 -0700

    working on skills section ;

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import ReactTooltip from 'react-tooltip';
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion';
+import { AiFillEye } from 'react-icons/ai';
 
-import {AppWrap} from '../../wrapper/wrapper'
-import {urlFor, client} from '../../client'
+import resumeImg from '../../assets/resume.jpg';
+import { AppWrap } from '../../wrapper/wrapper';
+import { urlFor, client } from '../../client';
 
 import './Skills.scss';
+import '../Work/Work.scss';
 
 const Skills = () => {
   const [experiences, setExperiences] = useState([]);
@@ -28,62 +31,67 @@ const Skills = () => {
     <>
       <h2 className='head-text'>Skills & Experience</h2>
 
-      <div className="app__skills-container">
-        <motion.div className="app__skills-list">
-          {skills && skills.map((skill) => (
-            <motion.div
-              whileInView={{ opacity: [0, 1] }}
-              transition={{ duration: 0.5 }}
-              className="app__skills-item app__flex"
-              key={skill.name}
-            >
-              <div
-                className="app__flex"
-                style={{ backgroundColor: skill.bgColor }}
+      <div className='app__skills-container'>
+        <motion.div className='app__skills-list'>
+          {skills &&
+            skills.map((skill) => (
+              <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className='app__skills-item app__flex'
+                key={skill.name}
               >
-                <img src={urlFor(skill.icon)} alt={skill.name} />
-              </div>
-              <p className="p-text">{skill.name}</p>
-            </motion.div>
-          ))}
-        </motion.div>
-        <div className="app__skills-exp">
-          {experiences && experiences.map((experience) => (
-            <motion.div
-              className="app__skills-exp-item"
-              key={experience.year}
-            >
-              <div className="app__skills-exp-year">
-                <p className="bold-text">{experience.year}</p>
-              </div>
-              <motion.div className="app__skills-exp-works">
-                {experience.works.map((work) => (
-                  <>
-                    <motion.div
-                      whileInView={{ opacity: [0, 1] }}
-                      transition={{ duration: 0.5 }}
-                      className="app__skills-exp-work"
-                      data-tip
-                      data-for={work.name}
-                      key={work.name}
-                    >
-                      <h4 className="bold-text">{work.name}</h4>
-                      <p className="p-text">{work.company}</p>
-                    </motion.div>
-                    <ReactTooltip
-                      id={work.name}
-                      effect="solid"
-                      arrowColor="#fff"
-                      className="skills-tooltip"
-                    >
-                      {work.desc}
-                    </ReactTooltip>
-                  </>
-                ))}
+                <div
+                  className='app__flex'
+                  style={{ backgroundColor: skill.bgColor }}
+                >
+                  <img src={urlFor(skill.icon)} alt={skill.name} />
+                </div>
+                <p className='p-text'>{skill.name}</p>
               </motion.div>
-            </motion.div>
-          ))}
-        </div>
+            ))}
+        </motion.div>
+
+        <motion.div
+          // animate={animateCard}
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
+          className='app__work-portfolio'
+        >
+          <div className='app__work-item app__flex'>
+            <div className='app__work-img app__flex'>
+              <img src={resumeImg} alt='resume-img' />
+
+              <a
+                href={
+                  'https://docs.google.com/document/d/1sKVvvqiWqA_lYPkbfFr4cIbze5EvfHacN-iO_w04ZB0/edit?usp=sharing'
+                }
+                target='_blank'
+                rel='noreferrer'
+              >
+                <motion.div
+                  whileHover={{ opacity: [0, 1] }}
+                  transition={{
+                    duration: 0.25,
+                    ease: 'easeInOut',
+                    staggerChildren: 0.5,
+                  }}
+                  className='app__work-hover app__flex'
+                ></motion.div>
+              </a>
+            </div>
+
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>Resume</h4>
+              <p className='p-text' style={{ marginTop: 10 }}>
+                Check out my Resume
+              </p>
+
+              <div className='app__work-tag app__flex'>
+                <p className='p-text'>Go to Link</p>
+              </div>
+            </div>
+          </div>
+        </motion.div>
       </div>
     </>
   );
