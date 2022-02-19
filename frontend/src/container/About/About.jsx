d1fccc04c84ea66b38e3173be3a0c3dd500c47f3
mosesvk <kaumatule93@gmail.com>
2022-02-18 17:46:50 -0700

    update

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -9,34 +9,27 @@ const abouts = [
   {
     title: 'Web Development',
     description: 'I am a good web developer',
-    imgUrl: '',
+    imgUrl: images.about01,
   },
-  { title: 'Web Design', description: 'I am a good web developer', imgUrl: '' },
-  { title: 'UI/UX', description: 'I am a good web developer', imgUrl: '' },
-  {
-    title: 'Web Animations',
-    description: 'I am a good web developer',
-    imgUrl: '',
-  },
+  { title: 'Web Design', description: 'I am a good web developer', imgUrl: images.about02 },
+  { title: 'UI/UX', description: 'I am a good web developer', imgUrl: images.about03}
 ];
 
 const About = () => {
   return (
     <>
       <h2 className='head-text'>
-        I know
-        <span>Good Design</span>
-        means
-        <span>Good Business</span>
+        I Know That <span>Good Design </span>
+        means <span>Good Business</span>
       </h2>
 
-      <div className='app_profiles'>
+      <div className='app__profiles'>
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: 'tween' }}
-            className='app_profile-item'
+            className='app__profile-item'
             key={about.title + index}
           >
             <img src={about.imgUrl} alt={about.title}/>
