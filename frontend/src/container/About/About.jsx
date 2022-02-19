2d5115cfbe516ee53aa873a8fe50bc0afd130b85
mosesvk <kaumatule93@gmail.com>
2022-02-19 09:44:33 -0700

    adding client.js to connect client with Sanity backend

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -2,20 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { images } from '../../constants/constants';
+import { urlFor, client } from '../../client';
 
 import './About.scss';
 
-const abouts = [
-  {
-    title: 'Web Development',
-    description: 'I am a good web developer',
-    imgUrl: images.about01,
-  },
-  { title: 'Web Design', description: 'I am a good web developer', imgUrl: images.about02 },
-  { title: 'UI/UX', description: 'I am a good web developer', imgUrl: images.about03}
-];
-
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className='head-text'>
@@ -32,9 +33,13 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title}/>
-            <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
-            <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='p-text' style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
           </motion.div>
         ))}
       </div>
